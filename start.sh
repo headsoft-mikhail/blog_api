11a50d6ca589a8978f95072254505675d8e23ddc
HeadSoft <a.mike@bk.ru>
2022-04-22 18:48:51 +0300

    fixed celery starting in docker, added README.md

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@ echo "MAKEMIGRATIONS"
 python manage.py makemigrations
 echo "MIGRATE"
 python manage.py migrate
+echo "START CELERY"
+celery -A blog_api worker -l info --detach
 echo "START GUNICORN"
 gunicorn blog_api.wsgi -w 4 -b 0.0.0.0:8000
